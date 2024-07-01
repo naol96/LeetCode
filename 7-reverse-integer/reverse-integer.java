18767f52d0854dc4321f0a12e380b2938d6da03e
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-01 14:25:16 +0300

    Time: 1 ms (75.40%) | Memory: 40.7 MB (58.12%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,31 +1,21 @@
 class Solution {
     public int reverse(int x) {
-        int y = x;
-       if(x < 0){
-        x *=-1;
-       }
-        String temp = Integer.toString(x);
-        char [] str = temp.toCharArray();
-        char [] reversedx = new char[str.length];
-        int j = 0;
-        for ( int i = str.length-1 ; i >=0 ; i --){
-            reversedx [j] = str [i];
-            j++; 
-
-        }
-        temp = String.valueOf(reversedx);
-        try{
-        int result = Integer.parseInt(temp);
-        if(y<0){
-            result*=-1;
+        int result = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
             
+           
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+            
+            result = result * 10 + digit;
         }
         return result;
-        } 
-        catch(Exception e){
-            return 0;
-        }
-        
     }
-    
 }
+
