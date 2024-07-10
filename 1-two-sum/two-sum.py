d8c5f5927b4a4be283ac991dd1ecd08dffa3fa84
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-10 10:07:24 +0300

    Time: 34 ms (89.31%) | Memory: 12.5 MB (66.16%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,5 +1,11 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
         num_map = {}
         for i, num in enumerate(nums):
             complement = target - num
