04e8fb205f3de53d046465bf086c253ee95a83fe
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-10 10:06:42 +0300

    :ballot_box_with_check: two-sum [Runtime: 62 ms; Memory: 17.7 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,11 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map = {}
         for i, num in enumerate(nums):
             complement = target - num
