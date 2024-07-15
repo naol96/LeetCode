2435f5cc141b3d2e972c1cc3074e67a821768bcf
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-15 11:55:23 +0300

    Time: 607 ms (40.63%) | Memory: 22.4 MB (86.54%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -5,11 +5,8 @@ class Solution:
         for items in points:
             dist = math.sqrt(items[0]**2 + items[1]**2)
             distance.append((dist, items))
-        
         distance.sort(key=lambda x: x[0])
-
         for item in distance[:k]:
             output.append(item[1])
-        
         return output
 
