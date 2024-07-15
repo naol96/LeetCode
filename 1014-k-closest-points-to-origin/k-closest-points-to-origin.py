b2b5fdf77abbad3219d9d57f2ce7bcf7679ac214
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-15 12:02:58 +0300

    Time: 589 ms (65.84%) | Memory: 22.7 MB (76.50%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,5 +1,5 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         points.sort(key=lambda pts : (pts[0] ** 2) + (pts[1] ** 2))
-        return points[:k]
+        return points[0:k]
 
