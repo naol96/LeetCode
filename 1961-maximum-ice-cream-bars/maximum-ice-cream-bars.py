0005b972f8b45b692a7dbd3a7eb3f73b246aadfc
naol96 <122468511+naol96@users.noreply.github.com>
2024-07-15 11:27:19 +0300

    Time: 634 ms (57.54%) | Memory: 30.5 MB (25.36%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        li=sorted(costs)
+        sum=0
+        result=0
+        for i in li:
+            sum+=i
+            if sum <= coins:
+                result+=1
+            if sum == coins:
+                break
+        return int(result)
+        
